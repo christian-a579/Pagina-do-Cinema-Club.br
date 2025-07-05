f246b852fa4f6c136f06bac7a14d7477fe2e6917
christian <ca3072907@gmail.com>
2025-07-05 17:05:52 -0300

    Construindo o front

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  // Fechar menu quando clicar fora
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar')) {
+      this.closeMenu();
+    }
+  }
+
+  // Fechar menu ao pressionar ESC
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      this.closeMenu();
+    }
+  }
+}
